c3de7d935a10776d068d94cf9ab88c0ef951218a
Eric Karge <eric.karge@innoq.com>
2017-08-02 18:30:46 +0200

    Allow to pass key as commandline argument to transcoder

--- src/main/java/org/hypoport/jwk/transcoder/JwkToPemTranscoder.java
+++ src/main/java/org/hypoport/jwk/transcoder/JwkToPemTranscoder.java
@@ -2,25 +2,31 @@ package org.hypoport.jwk.transcoder;
 
 import com.nimbusds.jose.jwk.AssymetricJWK;
 import com.nimbusds.jose.jwk.JWK;
-import org.apache.commons.io.IOUtils;
+import net.minidev.json.JSONObject;
+import net.minidev.json.JSONValue;
 import org.bouncycastle.openssl.jcajce.JcaPEMWriter;
 
-import java.io.InputStreamReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.OutputStreamWriter;
-import java.io.StringWriter;
 import java.security.Key;
+import java.util.Objects;
 
+import static java.lang.System.in;
 import static java.lang.System.out;
 import static java.nio.charset.StandardCharsets.US_ASCII;
-import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class JwkToPemTranscoder {
 
   public static void main(String[] argv) throws Exception {
-    InputStreamReader reader = new InputStreamReader(System.in, UTF_8);
-    StringWriter writer = new StringWriter();
-    IOUtils.copy(reader, writer);
-    JWK jwk = JWK.parse(writer.toString());
+    JWK jwk;
+    if (argv.length == 0 || Objects.equals(argv[0], "-")) {
+      jwk = JWK.parse((JSONObject) JSONValue.parse(in));
+    } else if (argv[0].matches("^[./~]")) {
+      jwk = JWK.parse((JSONObject) JSONValue.parse(new FileReader(new File(argv[0]))));
+    } else {
+      jwk = JWK.parse((JSONObject) JSONValue.parse(argv[0]));
+    }
     if (jwk instanceof AssymetricJWK) {
       Key key = ((AssymetricJWK) jwk).toPrivateKey();
       if (key == null) {
@@ -30,7 +36,7 @@ public class JwkToPemTranscoder {
         pemWriter.writeObject(key);
       }
     } else {
-      throw new UnsupportedOperationException("Incompatible JWK");
+      throw new UnsupportedOperationException("Incompatible JWK type: " + jwk.getKeyType());
     }
   }
 }
